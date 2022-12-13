76dc6bea34cc53b3f89d3be88e8df6674201abe5
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 17:35:59 -0400

    feature: name, description, language and fork count displaying on page from repos.tsx

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 const Repos = (): JSX.Element => {
-  useEffect( () => {
+  const [repo, setRepo] = useState([]);
+
+  useEffect(() => {
     // get the data from the backend api
     const api = async () => {
-    const res = await axios.get('http://localhost:4000/repos');
-    return res
-  } 
+      const res = await axios.get('http://localhost:4000/repos');
+      return res;
+    };
 
-      // call the function to display the data
-      api()
-        .then(console.log)
-  }, [])
+    // call the function to display the data
+    api().then((data) => {
+      setRepo(data.data);
+    });
+  }, []);
 
-  return (
-    <div>test</div>
-  )
-}
+  return <div>{repo.length && repo.map((r: any) => (
+  <>
+  <p>{r.name}</p>
+  <p>{r.description}</p>
+  <p>{r.language}</p>
+  <p>{r.forks_count}</p>
+  </>))}
+  </div>;
+};
 
-export default Repos
+export default Repos;
