b5d0cd8b6fe08b3ae6ed2618f22123b13517f215
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 17:08:15 -0400

    feature: data logging on React app

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -1,1 +1,23 @@
-import react from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+
+const Repos = (): JSX.Element => {
+  useEffect( () => {
+    // get the data from the backend api
+    const api = async () => {
+    const res = await axios.get('http://localhost:4000/repos');
+    return res
+  } 
+
+      // call the function to display the data
+      api()
+        .then(console.log)
+  }, [])
+
+  return (
+    <div>test</div>
+  )
+}
+
+export default Repos
