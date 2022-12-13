ad595ccfac2b3b12308296007e6964c2d9425e5a
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 18:15:31 -0400

    refactor: repo content import in App.tsx, parent element, from repos.tsx, child element

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Repos from './components/Repos';
 
 import './App.css';
 
 export function App() {
-  return ( <Repos />);
+  const [repo, setRepo] = useState([]);
+
+  useEffect(() => {
+    // get the data from the backend api
+    const api = async () => {
+      const res = await axios.get('http://localhost:4000/repos');
+      return res;
+    };
+
+    // call the function to display the data
+    api()
+      .then((data) => {
+        setRepo(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return ( <Repos repo={repo} />);
 }
--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 type RepoContent = {
   created_at: string;
   [key: string]: string | boolean | number | RepoContent;
 };
 
-const Repos = (): JSX.Element => {
-  const [repo, setRepo] = useState([]);
-
-  useEffect(() => {
-    // get the data from the backend api
-    const api = async () => {
-      const res = await axios.get('http://localhost:4000/repos');
-      return res;
-    };
-
-    // call the function to display the data
-    api()
-      .then((data) => {
-        setRepo(data.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+const Repos = ({repo}: {repo: RepoContent[]}): JSX.Element => {
 
   const sortedRepo = repo.sort((a: RepoContent, b: RepoContent) =>
     b.created_at.localeCompare(a.created_at)
