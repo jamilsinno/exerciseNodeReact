8928364dd3904c9f1fcf98c9ce7ff08a494de0af
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 16:03:43 -0400

    feature: json data displaying on page. Confirmed middleware functionality through refresh of page

--- src/controllers/repos.ts
+++ src/controllers/repos.ts
@@ -2,8 +2,10 @@ import { Request, Response } from 'express';
 import data from '../../data/repos.json';
 
 export const repoController = async (_req: Request, res: Response) => {
-    console.log(data);
+    // filter out the repositories that have a false fork key
+    const result = data.filter( d => !d.fork);
     res.header('Cache-control', 'no-store');
+
     // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-    res.status(200).json([])
+    res.status(200).json(result)
 }
