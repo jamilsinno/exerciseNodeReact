40ccc339c4bdcf0ae46409a0181d7a1e7bcd0e83
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 19:18:13 -0400

    bug: data showing on page load

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -1,21 +1,31 @@
-import {RepoContent} from '../types';
-const Repos = ({repo, lang}: {repo: RepoContent[], lang: string}): JSX.Element => {
-
+import { RepoContent } from '../types';
+const Repos = ({
+  repo,
+  lang,
+}: {
+  repo: RepoContent[];
+  lang: string;
+}): JSX.Element => {
   const sortedRepo = repo.sort((a: RepoContent, b: RepoContent) =>
     b.created_at.localeCompare(a.created_at)
   );
   return (
     <div>
-      {sortedRepo.length && 
-        sortedRepo.filter( r => r.language === lang).map((r: RepoContent, i: number) => (
-          <div key={i}>
-            <p>{r.name}</p>
-            <p>{r.description}</p>
-            <p>{r.language}</p>
-            <p>{r.forks_count}</p>
-            <p>{r.created_at}</p>
-          </div>
-        ))}
+      {sortedRepo.length &&
+        sortedRepo
+          .filter((r) => {
+            if (lang) return r.language === lang;
+            return true;
+          })
+          .map((r: RepoContent, i: number) => (
+            <div key={i}>
+              <p>{r.name}</p>
+              <p>{r.description}</p>
+              <p>{r.language}</p>
+              <p>{r.forks_count}</p>
+              <p>{r.created_at}</p>
+            </div>
+          ))}
     </div>
   );
 };
