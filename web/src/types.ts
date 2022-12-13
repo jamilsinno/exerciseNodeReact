661fd02ed0d3503e2ed99ae0659f7e0586882786
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 18:42:42 -0400

    refactor: RepoContent && Owner types

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,4 +1,46 @@
+export type Owner ={
+      login: string;
+      id: number;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean
+}
 export type RepoContent = {
+  id: number;
+  name: string;
+  full_name: string;
+  owner: Owner;
+  private: false;
+  html_url: string;
+  description: string;
+  fork: false;
+  url: string;
   created_at: string;
-  [key: string]: string | boolean | number | RepoContent;
+  updated_at: string;
+  pushed_at: string;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  commits_url: string;
+  homepage: string;
+  default_branch: string;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  forks_count: number;
+  forks: number;
 };
