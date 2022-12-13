6edf565628853e2f5767febf969a56adb9e648dc
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 15:57:54 -0400

    feature: data import to repos controller successful

--- src/controllers/repos.ts
+++ src/controllers/repos.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+import data from '../../data/repos.json';
 
 export const repoController = async (_req: Request, res: Response) => {
+    console.log(data);
     res.header('Cache-control', 'no-store');
     // TODO: See README.md Task (A). Return repo data here. You’ve got this!
     res.status(200).json([])
