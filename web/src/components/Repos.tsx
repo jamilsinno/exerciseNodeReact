70324dcdf564eedf3e5de0c589f2910b6a3b81f8
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 17:56:00 -0400

    feature: sorting repos using date feature working, wrong types

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -12,21 +12,25 @@ const Repos = (): JSX.Element => {
     };
 
     // call the function to display the data
-    api().then((data) => {
-      setRepo(data.data);
-    });
+    api()
+      .then((data) => {
+        setRepo(data.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
+  const sortedRepo = repo.sort((a: any, b: any) => b.created_at.localeCompare(a.created_at));
   return (
     <div>
-      {repo.length &&
-        repo.map((r: any) => (
-          <>
+      {sortedRepo.length &&
+        sortedRepo.map((r: any, i: number) => (
+          <div key={i}>
             <p>{r.name}</p>
             <p>{r.description}</p>
             <p>{r.language}</p>
             <p>{r.forks_count}</p>
-          </>
+            <p>{r.created_at}</p>
+          </div>
         ))}
     </div>
   );
