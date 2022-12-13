1fa2b11b97c65b2803ec9559f6e6bb70c48f98d1
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-13 15:47:55 -0400

    refactor: forgot to remove some unused variables in routes/repos

--- src/controllers/repos.ts
+++ src/controllers/repos.ts
@@ -2,5 +2,6 @@ import { Request, Response } from 'express';
 
 export const repoController = async (_req: Request, res: Response) => {
     res.header('Cache-control', 'no-store');
+    // TODO: See README.md Task (A). Return repo data here. You’ve got this!
     res.status(200).json([])
 }
