5aeec535861d66bbb810b26456d6c317e3982cc1
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-14 16:58:00 -0400

    chore: removal of repo backend files

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import { repoController } from '../controllers/repos';
-import { singleRepoController } from '../controllers/repo';
 
 export const repos = Router();
 
 repos.get('/', repoController);
-repos.get('/:id', singleRepoController);
