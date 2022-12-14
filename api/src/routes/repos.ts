e2e65ee8364ca4684f60cc3078348df8e59188c5
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-14 15:04:45 -0400

    feat: singleRepoController added to routes

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { repoController } from '../controllers/repos';
+import { singleRepoController } from '../controllers/repo';
 
 export const repos = Router();
 
 repos.get('/', repoController);
+repos.get('/:id', singleRepoController);
