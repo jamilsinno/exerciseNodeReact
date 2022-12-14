2cad8ef401403eeba803e26f897baa0aefc84e2f
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-14 17:01:11 -0400

    bug: cut off on commit date fix

--- web/src/components/Repo.tsx
+++ web/src/components/Repo.tsx
@@ -7,7 +7,7 @@ const Repo = ({ repo }: { repo: RepoContent[] }): any => {
   const res = repo.filter((d) => d.full_name === handle.id + '/' + handle.id2);
   return (
     <>
-      <p>Commit date: {res[0].updated_at.slice(0, 9)}</p>
+      <p>Commit date: {res[0].updated_at.slice(0, 10)}</p>
       <p>Author: {res[0].name}</p>
       <p>Message: {res[0].description}</p>
       <Link to="/">
